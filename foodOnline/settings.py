c568a51ea2eab3b9dee8b8b33edccc0527ba6031
Yahya-Azad <yahyaazad.1610@gmail.com>
2023-12-11 00:05:06 +0330

    first

--- foodOnline/settings.py
+++ foodOnline/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u33$!#+ws3t!pmmyvy517-(bnj=9-!h$5)qle9n0l-)1$x07bn'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
